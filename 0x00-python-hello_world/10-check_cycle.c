71828fc0ee89ad53910ed3ad9ad616644c9d46e4
Amadi <paulamadi39@gmail.com>
2022-11-28 10:43:05 -0800

    new corrections made

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,25 +7,17 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *tortoise;
-	listint_t *hare;
+	listint_t *tortoise = list;
+	listint_t *hare = list;
 
 	if (list == NULL)
 		return (0);
-	tortoise = list;
-	hare = list;
-	while (hare->next != NULL && hare->next->next != NULL)
+	while (tortoise && tortoise->next)
 	{
-		tortoise = tortoise->next;
-		hare = hare->next->next;
-		if (tortoise == hare)
+		hare = hare->next;
+		tortoise = tortoise->next->next;
+		if (hare == tortoise)
 		{
-			tortoise = list;
-			while (tortoise != hare)
-			{
-				tortoise = tortoise->next;
-				hare = hare->next;
-			}
 			return (1);
 		}
 	}
