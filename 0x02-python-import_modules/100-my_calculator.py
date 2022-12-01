3c3fbd0650b98cda9eb331fca2914825db46efa4
Amadi <paulamadi39@gmail.com>
2022-12-01 13:33:32 -0800

    correctiions to advanced task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     operator = sys.argv[2]
-    if operator = '+':
+    if operator == '+':
         result = add(a, b)
     elif operator == '-':
         result = sub(a, b)
