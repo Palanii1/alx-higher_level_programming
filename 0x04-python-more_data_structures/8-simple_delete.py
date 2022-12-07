5f156f00cc6e768b99bea3dabab83b21f4a6b9df
Amadi <paulamadi39@gmail.com>
2022-12-07 11:42:03 -0800

    more data structures of python

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/pyhton3
+def simple-delete9a_dictionary, key=""):
+    try:
+        del a_dictionary[key]
+        return (a_dictionary)
+    except KeyError:
+        return (a_dictionary)
