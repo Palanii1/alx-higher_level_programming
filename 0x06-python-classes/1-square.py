793e63a9d54babc0feb561312b04fa0797220184
Amadi <paulamadi39@gmail.com>
2022-12-20 07:04:23 -0800

    classes and objects task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A class Square that defines a square by a private instance attribute size"""
+
+class Square():
+    """defined square"""
+
+    def __init__(self, size):
+        """sets the required attributes for the square"""
+        self.__size = size
